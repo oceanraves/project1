606427cb075246e5ce1a4fc232286b8cfb08d9fe
Marikaohlsson <marikaeohlsson@gmail.com>
2021-09-14 18:58:18 +0200

    Shooting works

--- Course_1_TestProject/Assets/Project/Scripts/PlayerMovement.cs
+++ Course_1_TestProject/Assets/Project/Scripts/PlayerMovement.cs
@@ -9,35 +9,36 @@ public class PlayerMovement : MonoBehaviour
 
     private Vector3 moveInput;
     private Vector3 moveVelocity;
+    private Vector3 mousePosition;
+    private Vector3 screenPos;
 
     //private Camera mainCamera;
 
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody>();
-       // mainCamera = FindObjectOfType<Camera>();
+     
     }
 
     void Update()
     {
         moveInput = new Vector3(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"),0f);
         moveVelocity = moveInput * moveSpeed;
+        Look();
 
-       // Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
-       //Plane backgroundPlane = new Plane(Vector3.up,Vector3.zero);
-       // float rayLength;
-
-       // if(backgroundPlane.Raycast(cameraRay, out rayLength))
-        {
-           // Vector3 pointToLook = cameraRay.GetPoint(rayLength);
-           // Debug.DrawLine(cameraRay.origin, pointToLook, Color.blue);
-
-           // transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
-        }
     }
 
     private void FixedUpdate()
     {
         myRigidbody.velocity = moveVelocity;
+    }
+
+    private void Look()
+    {
+        
+
+
+
+
     }
 }
