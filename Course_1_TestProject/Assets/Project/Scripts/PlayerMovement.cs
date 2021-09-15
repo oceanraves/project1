d8456e4303c6b3315a0ecb44e3c8b9e51b8498cc
Marikaohlsson <marikaeohlsson@gmail.com>
2021-09-15 16:20:12 +0200

    added scripts
    added all the scripts and fixed firepoints

--- Course_1_TestProject/Assets/Project/Scripts/PlayerMovement.cs
+++ Course_1_TestProject/Assets/Project/Scripts/PlayerMovement.cs
@@ -4,22 +4,41 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float moveSpeed;
+    private Rigidbody myRigidbody;
+
+    private Vector3 moveInput;
+    private Vector3 moveVelocity;
+    private Vector3 mousePosition;
+    private Vector3 screenPos;
+
+    //private Camera mainCamera;
+
     void Start()
     {
-        
+        myRigidbody = GetComponent<Rigidbody>();
+
     }
 
     void Update()
     {
-        
+        moveInput = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
+        moveVelocity = moveInput * moveSpeed;
+        Look();
+
     }
 
-//INPUT
+    private void FixedUpdate()
+    {
+        myRigidbody.velocity = moveVelocity;
+    }
 
-    private void GetInput()
+    private void Look()
     {
 
+
+
+
+
     }
-
-
 }
