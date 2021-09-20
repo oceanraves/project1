910bc1d64e5530e46bcd18f1b8194f19758a3c14
Marikaohlsson <marikaeohlsson@gmail.com>
2021-09-20 10:53:25 +0200

    all enemies shoot

---
+++ Course_1_TestProject/Assets/Project/Scripts/Enemy02shooting.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy02shooting : MonoBehaviour
+{
+    [SerializeField]
+    GameObject enemyBullet;
+
+    public Transform firePoint;
+    public float bulletForce;
+    public float fireRate = 1f;
+    float nextFire;
+
+    void Start()
+    {
+        nextFire = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckFire();
+    }
+
+    void CheckFire()
+    {
+        if (Time.time > nextFire && firePoint != null)
+        {
+            GameObject eBullet = Instantiate(enemyBullet, firePoint.position, firePoint.rotation);
+            Rigidbody rb = eBullet.GetComponent<Rigidbody>();
+            rb.AddForce(firePoint.right * bulletForce, ForceMode.Impulse);
+            nextFire = Time.time + fireRate;
+        }
+    }
+}
